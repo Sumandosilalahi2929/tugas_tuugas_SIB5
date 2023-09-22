7e30942759ae86b9cf2dd511614194f09a27cb99
SumandoSilalahi2929 <143672248+Sumandosilalahi2929@users.noreply.github.com>
2023-09-22 23:03:24 +0700

    latihan php

--- latihan1.php
+++ latihan1.php
@@ -1,5 +1,28 @@
 <?php 
 
-echo"Hello World"
+//echo"Hello World";
 
+//latihan membuat variabel
+$namasiswa = "Sumando"; //tipedata string
+$umur = 25; //tipe data integer
+$beratbadan = 30.5; //tipe data float
+$_pekerjaan = 'Mahasiswa';
+
+$jari2 = 15;
+define('PHI', 3.14);
+$luas = PHI * $jari2 * $jari2;
+echo '<br>Nama Mahasiswa :  '.$namasiswa;
+echo '<br>Umur : '.$umur;
+echo '<br>Berat Badan : '.$beratbadan;
+echo '<br>Pekerjaan : '.$_pekerjaan;
+echo '<br> Berat Badan 2 : $beratbadan kg';
+echo "<br> Berat Badan 3 $beratbadan ";
+print '<br> Pekerjaan 2 : '.$_pekerjaan;
+
+//contoh contoh pemanggilan variable didalam tipe
 ?>
+
+<hr>
+<br> Siswa : 1 <br>Nama: <?= $namasiswa ?>
+<p> Umur : <?= $umur ?> </p>
+<p>Luas jari diatas adalah <?= $luas ?></p>
---
+++ latihan2.php
@@ -0,0 +1,28 @@
+<?php 
+$nama = "Sumando";
+$nilai = 100;
+if ($nilai >= 85 && $nilai <= 100) $grade = "A";
+else if ($nilai >= 75 && $nilai <=84)$grade = "B";
+else if ($nilai >= 60 && $nilai <= 74)$grade ="C";
+else if ($nilai >= 30 && $nilai <= 59)$grade = "D";
+else if ($nilai >=0 && $nilai <= 29)$grade = "E";
+else $grade = " ";
+
+switch ($grade){
+    case"A" : $predikat = "Memuaskan"; break;
+    case"B" : $predikat = "Baik"; break;
+    case"C" : $predikat = "Cukup"; break;
+    case"D" : $predikat = "Kurang"; break;
+    case"E" : $predikat = "Buruk"; break;
+    default: $predikat = ' ';
+
+}
+
+
+
+
+?>
+<h1>Nama Siswa : <?= $nama ?></h1>
+<h2>Nilai Siswa : <?= $nilai ?></h2>
+<h2> Grade : <?= $grade ?> </h2>
+<h2> Predikat : <?= $predikat ?> </h2>
