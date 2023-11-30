385a0239bb1fe67c8d17955e5eab9c63fbbea26f
SumandoSilalahi2929 <143672248+Sumandosilalahi2929@users.noreply.github.com>
2023-11-30 12:12:54 +0700

    Update profil user

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produk;
 
 class BerandaController extends Controller
 {
-    //
-    public function index(){
-        return view ('beranda');
+    public function index()
+    {
+        // Mengambil data produk dengan jenis_produk_id sebesar 
+        $produk = Produk::where('jenis_produk_id', 4)->get();
+
+        // Mengirim data produk ke tampilan beranda.blade.php
+        return view('beranda', ['produk' => $produk]);
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use DB;
+use Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facedes\Storage;
+// use App\Models\Pelanggan;
+
 
 class UserController extends Controller
 {
@@ -13,4 +18,54 @@ class UserController extends Controller
         $user = DB::table('users')->get();
         return view('admin.user.index', compact('user'));
     }
+    public function show(){
+        // $pelanggan = DB::table('pelanggan')->get();
+        $user = User::findOrFail(Auth::id());
+        return view('admin.user.profile', compact('user'));
+    }
+    
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|string|min:2|max:100',
+            'email' => 'required|email|unique:users,email,' . $id . ',id',
+            'old_password' => 'nullable|string',
+            'password' => 'nullable|required_with:old_password|string|confirmed|min:6',
+            'foto' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'role' => 'nullable|string'
+        ]);
+    
+        $user = User::find($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+    
+        if ($request->filled('old_password')) {
+            if (Hash::check($request->old_password, $user->password)) {
+                $user->password = Hash::make($request->password);
+            } else {
+                return back()
+                    ->withErrors(['old_password' => __('Tolong Periksa Password')])
+                    ->withInput();
+            }
+        }
+    
+        if ($request->hasFile('foto')) {
+            if ($user->foto && file_exists(public_path('uploads/foto/' . $user->foto))) {
+                // Hapus foto lama jika ada
+                unlink(public_path('uploads/foto/' . $user->foto));
+            }
+    
+            $file = $request->file('foto');
+            $fileName = $file->hashName();
+            $file->move(public_path('uploads/foto/'), $fileName);
+            $user->foto = $fileName;
+        }
+    
+        $user->role = $request->role;
+        $user->save();
+    
+        return back()->with('status', 'Profil berhasil diperbarui!');
+    }
+    
+    
 }
